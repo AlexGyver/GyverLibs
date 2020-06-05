830a59d8b22f1ab09ea4aad2ed191b1b446e3b50
Alex <beragumbo@ya.ru>
2020-06-05 13:40:53 +0300

    Create SmoothSweepTest.ino

---
+++ ServoSmooth/examples/SmoothSweepTest/SmoothSweepTest.ino
@@ -0,0 +1,38 @@
+/*
+   Данный код плавно двигает туда-сюда одной сервой (на пине 5)
+   Наблюдаем за графиком угла в плоттере (Инструменты/Плоттер по последовательному соединения)
+*/
+
+#include <ServoSmooth.h>
+ServoSmooth servo;
+
+uint32_t tmr;
+boolean flag;
+
+void setup() {
+  Serial.begin(9600);
+  servo.attach(5);        // подключить
+  servo.setSpeed(30);     // ограничить скорость
+  servo.setAccel(0.05);   // установить ускорение (разгон и торможение)
+  servo.setTargetDeg(100);
+  //servo.setDirection(REVERSE);
+  servo.write(180);
+}
+
+void loop() {
+  Serial.print(servo.getCurrentDeg());
+  Serial.print(' ');
+  Serial.print(servo.getTargetDeg());
+  Serial.print(' ');
+  Serial.println(servo.tick() * 10);
+  delay(10);
+
+
+  static uint32_t timer;
+  if (millis() - timer > 3000) {
+    static bool kek = false;
+    kek = !kek;
+    timer = millis();
+    servo.setTargetDeg(kek ? 10 : 50);
+  }
+}
