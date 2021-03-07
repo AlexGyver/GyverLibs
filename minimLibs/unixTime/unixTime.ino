209cb507c5edc402804e2da18483971b2fdf77d1
Alex <beragumbo@ya.ru>
2021-03-08 01:31:32 +0300

    Update unixTime.ino

--- minimLibs/unixTime/unixTime.ino
+++ minimLibs/unixTime/unixTime.ino
@@ -2,7 +2,7 @@
 
 void setup() {
   Serial.begin(9600);
-  UnixDate date(1615153965, 3);
+  UnixDate date(1615153965, 3);	// unix time, GMT
   Serial.println(date.year);
   Serial.println(date.month);
   Serial.println(date.day);
