4a36232befa503eace0c0446b2f6a8341e49b19a
Alex <beragumbo@ya.ru>
2021-04-02 14:46:17 +0300

    add

---
+++ minimLibs/telegramBot/FastBot.h
@@ -0,0 +1,104 @@
+// очень простая и быстрая библиотека для телеграм бота
+// оптимизирована для большой нагрузки (спокойно принимает 50 сообщ в секунду)
+
+#ifndef FastBot_h
+#define FastBot_h
+#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
+static std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+static HTTPClient https;
+
+/*
+  Статусы tick:
+  0 - ожидание
+  1 - ОК
+  2 - Переполнен по ovf
+  3 - Ошибка телеграм
+  4 - Ошибка подключения
+*/
+
+class FastBot {
+  public:
+    FastBot (String token, int limit = 10, int ovf = 10000, int period = 1000) {
+      _request = (String)"https://api.telegram.org/bot" + token + "/getUpdates?limit=" + limit + "&offset=";
+      _ovf = ovf;
+      _period = period;
+      client->setInsecure();
+    }
+    void setChatID(String chatID) {
+      chatIDflag = true;
+      _chatID = chatID;
+    }
+
+    void attach(void (*handler)(String&)) {
+      _callback = handler;
+    }
+
+    byte tick() {
+      byte status = 0;
+      if (millis() - tmr >= _period) {
+        tmr = millis();
+        if (https.begin(*client, _request + ID)) {
+          if (https.GET() == HTTP_CODE_OK) {
+            if (https.getSize() > _ovf) {
+              int IDpos = https.getString().indexOf("{\"update_id\":", IDpos);
+              if (IDpos > 0) ID = https.getString().substring(IDpos + 13, https.getString().indexOf(',', IDpos)).toInt();
+              ID++;
+              https.end();
+              return 2;
+            }
+            // считаем, сколько в пакете данных (метка update_id)
+            int count = 0;
+            int IDpos = 0;
+            while (1) {
+              IDpos = https.getString().indexOf("{\"update_id\":", IDpos);
+              if (IDpos > 0) {
+                ID = https.getString().substring(IDpos + 13, https.getString().indexOf(',', IDpos)).toInt() + 1;  // сразу ++ для следующего пакета
+                IDpos++;
+                count++;
+              } else break;
+            }
+
+            // считаем и парсим сообщения
+            if (count) {
+              int textPos = 0;
+              while (1) {
+                if (chatIDflag) {
+                  textPos = https.getString().indexOf("\"chat\":{\"id\":", textPos);
+                  if (textPos < 0) break;
+                  int endPos = https.getString().indexOf(",\"", textPos);
+                  String chatID = https.getString().substring(textPos + 13, endPos);
+                  textPos = endPos;
+                  if (!chatID.equals(_chatID)) continue;
+                }
+                textPos = https.getString().indexOf(",\"text\":\"", textPos);
+                if (textPos < 0) break;
+                int endPos = https.getString().indexOf("\"}}", textPos);
+                int endPos2 = https.getString().indexOf("\",\"entities", textPos);
+                if (endPos > 0 && endPos2 > 0) endPos = min(endPos, endPos2);
+                else if (endPos < 0) endPos = endPos2;
+                if (https.getString()[textPos + 9] == '/') textPos++;
+                String str = https.getString().substring(textPos + 9, endPos);
+                if (*_callback) _callback(str);
+                textPos = endPos;
+                yield();
+              }
+            }
+          } else status = 3;
+          https.end();
+        } else status = 4;
+      }
+      return status;
+    }
+
+  private:
+    void (*_callback)(String& str) = NULL;
+    String _request;
+    int _ovf = 5000, _period = 1000;
+    long ID = 0;
+    uint32_t tmr = 0;
+    bool chatIDflag = false;
+    String _chatID = "";
+};
+#endif
---
+++ minimLibs/telegramBot/telegramBot.ino
@@ -0,0 +1,46 @@
+#define WIFI_SSID "login"
+#define WIFI_PASS "pass"
+#define BOT_TOKEN "2654326546:asjhAsfAsfkllgUsaOuiz_axfkj_AsfkjhB"
+#define CHAT_ID "574578754"
+
+
+#include <ESP8266WiFi.h>
+#include "FastBot.h"
+FastBot bot(BOT_TOKEN);
+
+//FastBot bot(токен, лимит, порог, период);
+// токен - уникальный код бота, берётся у BotFather
+// лимит - количество сообщений, получаемое из одного запроса (по умолч. 10)
+// порог - количество символов, при котором API запрос будет считаться слишком большим и будет пропущен (по умолч. 10000)
+// период - период автоматического опроса бота в мс (по умолч. 1000)
+
+void setup() {
+  delay(2000);
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected");
+
+  // установить ID чата, чтобы принимать сообщения только из него
+  // узнать ID можно из ручного запроса в браузере
+  //bot.setChatID(CHAT_ID);
+
+  // подключаем функцию-обработчик
+  bot.attach(parseHandler);
+}
+
+// создать свою функцию вида имя(String& строка)
+void parseHandler(String& str) {
+  // при вызове str примет значение сообщения в чате!
+  Serial.println(str);
+}
+
+void loop() {
+  // тикаем в луп
+  bot.tick();
+}
